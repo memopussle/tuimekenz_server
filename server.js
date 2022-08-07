7d7b6558a219688387c3b152bbfd727c33a2a1f5
thugiang <xanhtham.cuc@gmail.com>
2022-08-07 15:04:20 +1200

    fix: refactor hard-coded connection string to Heroku config var

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 const app = require("./app");
+const uri = process.env.MONGODB_URI;
 mongoose
-  .connect("mongodb://localhost:27017/mongo")
+  .connect("mongodb+srv://thugiang:gianganhthu7b..@cluster0.tupwf3g.mongodb.net/mongo")
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
