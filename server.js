61084bfebd39a5daa6847540e4b5ba6af8b520ac
thugiang <xanhtham.cuc@gmail.com>
2022-08-07 17:08:45 +1200

    trying to fix the data not showing on heroku when node stops running

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 const app = require("./app");
 const uri = process.env.MONGODB_URI;
-mongoose.connect(
-  "mongodb+srv://thugiang:gianganhthu7b..@cluster0.tupwf3g.mongodb.net/mongo?retryWrites=true&w=majority",
-  { useNewUrlParser: true }
-);
+mongoose
+  .connect(
+    "mongodb+srv://thugiang:gianganhthu7b..@cluster0.tupwf3g.mongodb.net/mongo?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
+  )
+  .then(() => {
+    console.log("Connected to Mongo database");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 app.listen(port, () => {
   console.log(`App listening on port 5000`);
