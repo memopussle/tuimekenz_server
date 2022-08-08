76e6931dd9a271b8cfdccfab1a8076dd1b37b35d
thugiang <xanhtham.cuc@gmail.com>
2022-08-08 12:14:10 +1200

    fixing client side and now pushing to heroku again

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const tours = require("./model/TourModel");
 const validId = require("./utils/validId");
 const path = require("path");
+require("dotenv").config({ path: "./config.env" }); 
 
 app.use(cors());
 app.use(express.json());
@@ -18,23 +19,31 @@ app.get("/tours/:id", async (req, res) => {
   const idParam = req.params.id;
   const itIsValidId = validId(idParam);
 
-  
   if (itIsValidId === false) {
     return res.status(400).send({ message: "id provided is invalid" });
   }
 
   const tourById = await tours.findById(idParam);
-   if (!tourById) {
-     return res.status(404).send({ message: "id not found" });
-   }
-
+  if (!tourById) {
+    return res.status(404).send({ message: "id not found" });
+  }
 
   return res.status(200).send(tourById);
 });
 
+//app.use(express.static(path.join(__dirname + "/public")));
 
+//middle ware
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "travel/client/build")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "travel", "client" "build", "index.html"));
+//   });
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("Api running");
+//   });
+// }
 app.use(express.static(path.join(__dirname + "/public")));
-
-
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const dotenv = require("dotenv");
+const path = require("path");
 const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 const app = require("./app");
-const uri = process.env.MONGODB_URI;
+
 mongoose
   .connect(
     "mongodb+srv://thugiang:gianganhthu7b..@cluster0.tupwf3g.mongodb.net/mongo?retryWrites=true&w=majority",
@@ -13,6 +15,8 @@ mongoose
   .catch((err) => {
     console.error(err);
   });
+
+
 
 app.listen(port, () => {
   console.log(`App listening on port 5000`);
